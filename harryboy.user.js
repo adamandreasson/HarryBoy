adb6aef6001d30af71c104894b5c2d18dd6d527c
Unknown <sdisoriented@gmail.com>
2018-01-17 00:34:16 +0100

    Also bump the version, oops

--- harryboy.user.js
+++ harryboy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Harry Boy
 // @namespace    https://adamandreasson.se/
-// @version      1.0.5
+// @version      1.0.6
 // @description  Vinn på travet med Harry Boy! PS. Du måste synka med discord för att få notifikationer när saker händer, skriv !travet [travian namn] i #memes chatten
 // @author       Adam Andreasson
 // @match        https://tx3.travian.se/*
